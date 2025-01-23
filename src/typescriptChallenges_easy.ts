714e8fdde3ab5b375e2b950a1a58f4d0adb4c55e
malg266526 <tylek.malgorzata@interia.pl>
2025-01-23 11:58:10 +0100

    typescript tasks

--- src/typescriptChallenges_easy.ts
+++ src/typescriptChallenges_easy.ts
@@ -42,3 +42,36 @@ type If<C, T, F> = C extends true ? T : F
 // Concat
 
 type Concat<T extends readonly unknown[], U extends readonly unknown[]> = [...T, ...U]
+
+// Includes
+
+type IsBoolean<T extends boolean> = (true  | false) extends T ? true : false;
+type b11 = IsBoolean<boolean>;
+type b12 = IsBoolean<true>;
+type b13 = IsBoolean<false>;
+
+type isExact<T, U> = [T] extends [U] ? [U] extends [T] ? true : false :false;
+
+// failuje dla readonly - non readonly
+
+type Includes<T extends any[], U> =
+    T extends [] ? false : T extends [infer First, ...infer Rest] ?
+        isExact<U, T[0]> extends true ? true : Includes<Rest, U> : never;
+
+// not always working - problems with boolean & true/false
+
+type Includes2<T extends readonly any[], U> = {
+    [P in T[number]]: true
+}[U] extends true ? true : false;
+
+// Push
+
+type Push<T extends any[], U> = [...T, U]
+
+// Unshift
+
+type Unshift<T extends unknown[], U> = [U, ...T]
+
+// Parameters
+
+type MyParameters<T extends (...args: any[]) => any> = T extends (...any: infer S) => any ? S : never
